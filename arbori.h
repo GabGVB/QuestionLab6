c18da0ae951e8311fa3a120cd4ab22ebc696f831
GabGVB <voicu.gabriel2601@gmail.com>
2023-04-09 20:27:27 +0300

    include FindPath si AfisPath in .h

--- arbori.h
+++ arbori.h
@@ -11,3 +11,5 @@ typedef struct N Node;
 Node* createNode(char type, int num);
 Node* createTree(Node *root);
 void preorder ( Node * root );
+void AfisPath (char *v, int n);
+void FindPath(Node *root,char *v,char c,int n);
