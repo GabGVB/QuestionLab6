b3a253b348e53540e97a23853fbdb6eafcea9721
GabGVB <voicu.gabriel2601@gmail.com>
2023-04-10 13:17:15 +0300

    Realizare FindPathFor (+aux) dinamic in main/test

--- test.c
+++ test.c
@@ -6,31 +6,37 @@ typedef struct
     char *path;
 } PathsForEach;
 
-struct Answers
+typedef struct
 {
     int nrCai;
     int Type;
-    PathsForEach caiTo[5];
-} C[5];
+    PathsForEach *caiTo;
+} Answers;
 int nrAns;
-void SaveIn (char *v,int lenPath, int AnswerType)
+void SaveIn (char *v,int lenPath, int AnswerType, Answers **C)
 {
     int i=0,j;
-    while (i<nrAns && C[i].Type!=AnswerType)
+    while (i<nrAns && (*C)[i].Type!=AnswerType)
         i++;
     if (i==nrAns)
     {
         nrAns++;
-        C[i].Type=AnswerType;
+        *C=(Answers*)realloc(*C,nrAns*sizeof(Answers));
+        (*C)[i].caiTo=(PathsForEach*)malloc(sizeof(PathsForEach));
+        (*C)[i].Type=AnswerType;
+        (*C)[i].nrCai=0;
     }
-    C[i].caiTo[C[i].nrCai].path=(char*)malloc(lenPath*sizeof(char));
-    C[i].caiTo[C[i].nrCai].lenPath=lenPath;
+    else
+        (*C)[i].caiTo=(PathsForEach*)realloc((*C)[i].caiTo,((*C)[i].nrCai+1)*sizeof(PathsForEach));
+
+    (*C)[i].caiTo[(*C)[i].nrCai].path=(char*)malloc(lenPath*sizeof(char));
+    (*C)[i].caiTo[(*C)[i].nrCai].lenPath=lenPath;
 
     for (j=0; j<lenPath; j++)
-        C[i].caiTo[C[i].nrCai].path[j]=v[j];
-        C[i].nrCai++;
+        (*C)[i].caiTo[(*C)[i].nrCai].path[j]=v[j];
+    (*C)[i].nrCai++;
 }
-void FindPathFor(Node *root,char *v, char c,int n)
+void FindPathFor(Node *root,char *v, char c,int n, Answers **C)
 {
 
     if (c!='0')
@@ -39,12 +45,12 @@ void FindPathFor(Node *root,char *v, char c,int n)
     if (root->type=='C')
     {
 
-        SaveIn(v,n+1,root->num);
+        SaveIn(v,n+1,root->num,C);
     }
     else
     {
-        FindPathFor(root->left,v,'d',n+1);
-        FindPathFor(root->right,v,'n',n+1);
+        FindPathFor(root->left,v,'d',n+1,C);
+        FindPathFor(root->right,v,'n',n+1,C);
     }
 
 
@@ -52,11 +58,14 @@ void FindPathFor(Node *root,char *v, char c,int n)
 int main()
 {
     int i,j,k;
+    Answers *C;
+    C=(Answers*)calloc(1,sizeof(Answers));
     char v[20];
     Node *root;
     root=createTree(root);
     //FindPath(root,v,'0',0);
-    FindPathFor(root,v,'0',0);
+    FindPathFor(root,v,'0',0,&C);
+
     for (i=0; i<nrAns; i++)
     {
 
